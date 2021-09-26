f580f57908d088744d221e3bb73c1ef68a779dda
robeyoul-awal-raju <robeyoulawal@gmail.com>
2021-09-26 11:30:44 +0600

    header section update

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -4,7 +4,10 @@ import './Header.css';
 export const Header = () => {
   return (
     <div className="header">
-      <h1 className="main-heading">Do you need any Devloper ?</h1>
+      <h1 className="main-heading">Do you need any Developer ?</h1>
+      <h3 className="seconday-heading">
+        Your minimum budget must be <span className="budget">$15</span>
+      </h3>
     </div>
   );
 };
